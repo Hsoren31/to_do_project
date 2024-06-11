5e947c228078eb8333e0cada3fe2c87b18cf4a9f
Haley Sorensen <sorensenhaley@gmail.com>
2024-06-11 15:44:50 -0600

    Add date and priority level to task form

--- src/todos.js
+++ src/todos.js
@@ -3,8 +3,10 @@ import { pushToArray } from "./lists";
 const todoDiv = document.querySelector('.tasks');
 const todos = [];
 
-function TodoObj(title) {
+function TodoObj(title, date, priority) {
     this.title = title;
+    this.date = date;
+    this.priority = priority;
 }
 
 function renderTodo(){
@@ -19,6 +21,8 @@ function renderTodo(){
                     id='task-${i}'
                 />
                 <label for='task-${i}'>${todo.title}</label>
+                <p>${todo.date}</p>
+                <p>${todo.priority}</p>
             </div>`
         );
 
@@ -29,7 +33,10 @@ function renderTodo(){
 
 function todoHandler(){
     let taskTitle = document.querySelector('.new_task').value;
-    let task = new TodoObj(taskTitle);
+    let taskDate = document.querySelector('#date').value;
+    let taskPriority = document.querySelector('#priority').value;
+
+    let task = new TodoObj(taskTitle, taskDate, taskPriority);
     pushToArray(todos, task);
     renderTodo();
 }
