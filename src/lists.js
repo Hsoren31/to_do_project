8d9b4bccb82d868843ee4eb6e2ffbdf557a77a86
Haley Sorensen <sorensenhaley@gmail.com>
2024-06-12 13:18:44 -0600

    Identify lists when clicked on, rewrite renderFolders

--- src/lists.js
+++ src/lists.js
@@ -1,33 +1,39 @@
-const folderDiv = document.querySelector('.folders');
+const folderDiv = document.querySelector('#folders');
+
 const folders = [];
 
-function FolderObj(folderName) {
-    this.folderName = folderName;
-}
+function FolderObj(name) {
+   return { id: Date.now(), name: name}
+};
 
 function pushToArray(array, item){
     array.push(item);
 }
 
 function renderFolders(){
-    folderDiv.innerText = '';
+    clearElement(folderDiv);
     folders.forEach((folder) =>{
-        let currentFolderDiv = folderDiv.innerHTML;
-        folderDiv.innerHTML = '';
-        let displayFolder = (
-            `<li>${folder.folderName}</li>`
-        );
-        
-        let amendedFolderDiv = currentFolderDiv + displayFolder;
-        folderDiv.innerHTML = amendedFolderDiv;
+        const folderElement = document.createElement('li');
+        folderElement.dataset.listId = folder.id;
+        folderElement.classList.add('list-name');
+        folderElement.innerText = folder.name;
+        folderDiv.appendChild(folderElement);
     });
+};
+
+function clearElement(element){
+    while(element.firstChild){
+        element.removeChild(element.firstChild);
+    };
 };
 
 function folderHandler(){
     let folderName = document.querySelector('.new_list').value;
+    if (folderName == null || folderName === '') return;
     let folder = new FolderObj(folderName);
     pushToArray(folders, folder);
     renderFolders();
 };
+
 
 export { pushToArray, folderHandler }
